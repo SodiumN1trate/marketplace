3f09b470ebaabb03dc21f4451f95f92aa982bfac
Fleksis <markuss0303@gmail.com>
2022-04-04 18:54:52 +0300

    code template

--- Ebay_parody/Ebay_parody/Product.cs
+++ Ebay_parody/Ebay_parody/Product.cs
@@ -6,6 +6,24 @@ using System.Threading.Tasks;
 
 namespace Ebay_parody {
     class Product {
+        private int ID;
+        private string title;
+        private string description;
+        private decimal price;
 
+        public string Title {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public string Description {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public decimal Price {
+            get { return price; }
+            set { price = value; }
+        }
     }
 }
--- Ebay_parody/Ebay_parody/User.cs
+++ Ebay_parody/Ebay_parody/User.cs
@@ -4,8 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ebay_parody {
-    class User {
+using System;
 
+class User {
+    private int ID;
+    private string firstname;
+    private string surname;
+    private decimal balance;
+    private Product productForSale;
+
+    public string Firstname {
+        get { return firstname; }
+        set { firstname = value; }
+    }
+
+    public string Surname {
+        get { return surname; }
+        set { surname = value; }
+    }
+
+    public decimal Balance {
+        get { return balance; }
+        set { balance = value; }
+    }
+
+    public Product ProductForSale {
+        get { return productForSale; }
+        set { productForSale = value; }
     }
 }
